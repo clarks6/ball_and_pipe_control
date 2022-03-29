afd16c7a7cdb1a51ea11ddadd38f7470269ede84
Ronan Harkins <harkin77@students.rowan.edu>
2022-03-29 11:52:52 -0400

    Transfer Function with variables Defined

--- Eviroment.m
+++ Eviroment.m
@@ -1,13 +1,21 @@
 %code to start the Enviroment
+timesample=0.5; 
+g=9.8;        %Gravity
+m= 1.4/1e6;   %mass of the ball
+rho=1.225;    %Rho
+V=6.55483e-5; %Volume 
+Veq=2.4384;   %
 
-g=9.8;
-m= 1.4/1e6;
-rho=1.225;
-V=6.55483e-5;
-Veq=2.4384;
+C2=(((2*g)/(Veq))*((M-(rho*V))/m)); % value of C2
+C3=6.3787*10^(-4);                  %Value of C3
 
-C2=(((2*g)/(Veq))*((M-(rho*V))/m));
-C3=6.3787*10^(-4);
+TF= ((C3*C2)/(s*(s+C2)+(C3*C2)));   %Unity Feedback
+sys= ss(TF);                        
 
-system= ((C3*C2)/(s*(s+C2)+(C3*C2)));
-sys= ss(system);
+lsim(sys,pwm,timesample); 
+
+
+
+
+
+
