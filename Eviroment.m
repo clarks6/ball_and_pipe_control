cd5fe349c00f94d9e428c4f672e0017027b43223
Ronan-Harkins <harkin77@students.rowan.edu>
2022-04-05 17:34:18 -0400

    Fixed Enviroment 4/5/2022

--- Eviroment.m
+++ Eviroment.m
@@ -1,21 +1,26 @@
-%code to start the Enviroment
-timesample=0.5; 
+syms  s
+
+%%input environment
+pwm=[3000;3000];
+timesample=[0;0.25]; 
+
+%%constants
+x0= [];
 g=9.8;        %Gravity
-m= 1.4/1e6;   %mass of the ball
-rho=1.225;    %Rho
-V=6.55483e-5; %Volume 
-Veq=2.4384;   %
+mBall= 0.01;   %mass of the ball
+rhoAir=1.225;    %Rho
+vBall=6.55483e-5; %Volume 
+vEq=2.4384;   %
 
-C2=(((2*g)/(Veq))*((M-(rho*V))/m)); % value of C2
-C3=6.3787*10^(-4);                  %Value of C3
-
-TF= ((C3*C2)/(s*(s+C2)+(C3*C2)));   %Unity Feedback
+%%equation
+%C2=(((2*g)/(Veq))*((m-(rho*V))/m)); % value of C2
+%C3=6.3787*10^(-4);                  %Value of C3
+%G = ((C3*C2)/(s*(s+C2)+(C3*C2)));   %Unity Feedback
+c2 = (2*g*(mBall-rhoAir*vBall))/(vEq*mBall);
+c3 = 6.3787 * 10^-4;
+G = (c3*c2)/(s*(s+c2));
+[N,D] = numden(G);
+TF = tf(sym2poly(N),sym2poly(D));
 sys= ss(TF);                        
 
-lsim(sys,pwm,timesample); 
-
-
-
-
-
-
+[Y,X,~]= lsim(sys,pwm,timesample,x0);
